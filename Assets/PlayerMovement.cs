79c6d5b869c2167ff75c0610f9d4d957796253ee
yahay505 <58685802+yahay505@users.noreply.github.com>
2021-12-23 08:53:07 +0300

    It's time to go home

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,10 +1,36 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    // DONE: ground Check
+    // DONE: Gravity
+    // DONE: Grace period
 
+    private bool IsGrounded
+    {
+        get
+        {
+            return coyoteTime > 0;
+        }
+        set
+        {
+            coyoteTime =  (value ? 1 : coyoteTime - Time.deltaTime);
+
+            // if (value)
+            // {
+            //     coyoteTime =(int) (1 / Time.deltaTime);
+            // }
+            // else
+            // {
+            //     coyoteTime -=  1;
+            // }
+        }
+    }
+[SerializeField]
+    private float coyoteTime = 0;
+    public LayerMask mask;
+    public Transform left, right;
     public float Xspeed,Yspeed;
     private Rigidbody2D rb2d;
     // Start is called before the first frame update
@@ -13,9 +39,10 @@ public class PlayerMovement : MonoBehaviour
         rb2d = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
+    private float jumpasd;     // Update is called once per frame
     void Update()
     {
+        jumpasd -= Time.deltaTime;
         if (Input.GetKey(KeyCode.A))
         {
             rb2d.velocity = new Vector2(-Xspeed, rb2d.velocity.y);
@@ -25,10 +52,31 @@ public class PlayerMovement : MonoBehaviour
             rb2d.velocity = new Vector2(Xspeed, rb2d.velocity.y);
         }
 
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            rb2d.AddForce(new Vector2(0,Yspeed),ForceMode2D.Impulse);
+        IsGrounded = Physics2D.Raycast(left.position, Vector2.down, .3f, mask.value)
+                     || Physics2D.Raycast(right.position, Vector2.down, .3f, mask.value);
+                 
             
+            Debug.Log(IsGrounded);
+
+            
+        if (Input.GetKeyDown(KeyCode.Space)&&IsGrounded)
+        {
+            rb2d.AddForce(new Vector2(0,Yspeed-rb2d.velocity.y),ForceMode2D.Impulse);
+            coyoteTime = 0;
+            IsGrounded = false;
+            jumpasd = .5f;
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            if (IsGrounded)
+            {
+                rb2d.gravityScale = 3;
+            }
+        }
+        else
+        {
+            rb2d.gravityScale = 9;
         }
     }
 }
